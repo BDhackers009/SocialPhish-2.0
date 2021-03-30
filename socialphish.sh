ba7c26fc486d9e313c790de1ee797e0efc706a67
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2021-03-30 13:57:53 +0600

    update socialphish.sh

--- socialphish.sh
+++ socialphish.sh
@@ -7,8 +7,8 @@ trap 'printf "\n";stop;exit 1' 2
 
 dependencies() {
 
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install ii By executing apt install php -y"; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it By executing apt install wget -y 's; exit 1; }
 
 }
 
